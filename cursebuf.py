8c955a5f953007f495bf38c6741f38b05afa11fb
rlong <rob@buildtall.com>
2015-02-22 16:01:47 -0600

    first pass functioning scrolling

--- cursebuf.py
+++ cursebuf.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
 class cursebuf:
-    def __init__(self, lines):
-        self.lines = []
-        for n in range(lines):
-            self.lines.append("")
+    def __init__(self, max_lines, lines):
+        self.lines = [lines]
+        self.max_lines = max_lines
 
     def get_line(self, line):
         return self.lines[line]
 
     def add_line(self, line):
-        self.lines.pop()
+        if len(self.lines) >= self.max_lines:
+            self.lines.pop()
         self.lines.insert(0, line)
 
     def get_buf(self):
--- genome_scroller.py
+++ genome_scroller.py
@@ -33,14 +33,42 @@ class Camera(object):
         glLoadIdentity()
         gluOrtho2D(0, self.win.width, 0, self.win.height)
 
+class Line(object):
+    def __init__(self, fnt, text, x, y, color):
+        self.text_object = font.Text(
+            fnt,
+            text,
+            x=x,
+            y=y,
+            halign=font.Text.CENTER,
+            valign=font.Text.CENTER,
+            color=color,
+        )
+    def draw(self):
+        self.text_object.draw()
+
+    def increment(self):
+        self.text_object.y += 1
+
+
 
 class Hud(object):
 
     def __init__(self, win, input_file, line_width):
+        self.counter = 0
         self.tn = TextNozzle(input_file, line_width) 
         self.win = win
         self.helv = font.load('Helvetica', win.width / 150.0)
-        self.cb = cursebuf.cursebuf( 10 )
+        self.cb = cursebuf.cursebuf(100, self.get_text_object())
+
+    def get_text_object(self):
+        return Line(
+            self.helv,
+            self.pull_from_file(),
+            self.win.width/2,
+            -10,
+            (1,1,1,1)
+        ) 
 
     def pull_from_file(self):
         return self.tn.get_line()
@@ -50,14 +78,21 @@ class Hud(object):
             self.helv,
             self.pull_from_file(),
             x=self.win.width / 2,
-            y=self.win.height / 2,
+            y=-10,
             halign=font.Text.CENTER,
             valign=font.Text.CENTER,
             color=(1, 1, 1, 0.5),
         )
+    def increment(self):
+        self.counter += 1
 
     def draw_texts(self):
-        self.get_text().draw()
+        if self.counter > 15:
+            self.counter = 0
+            self.cb.add_line(self.get_text_object())
+        map(lambda text: text.increment(), self.cb.get_buf())
+        map(lambda text: text.draw(), self.cb.get_buf())
+        self.increment()
 
     def draw(self):
         glClear(GL_COLOR_BUFFER_BIT)
@@ -70,10 +105,10 @@ class App(object):
     def __init__(self, input_file):
         self.win = window.Window(fullscreen=True)
         self.camera = Camera(self.win, zoom=100.0)
-        self.hud = Hud(self.win, input_file, 100)
+        self.hud = Hud(self.win, input_file, 200)
 
     def mainLoop(self):
-        clock.set_fps_limit(6)
+        clock.set_fps_limit(60)
         while not self.win.has_exit:
             self.win.dispatch_events()
             self.camera.hudProjection()
